adb7baa3c2b746a53523e50f5de74ee5773efc92
Isaac Edmonds <ie1g15@soton.ac.uk>
2019-09-22 15:57:49 +0100

    Added Duplicant Life Cycle temp

---
+++ DuplicantLifecycle/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace DuplicantLifecycle
+{
+    public class Config
+{
+    [JsonProperty]
+    public bool ExampleConfigBool { get; set; } = false;
+}
+}
